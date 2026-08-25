a8b4fec2d6b59ef05b05766a73dd736cf00930a2
andres cabrera <afcabrera36@ucatolica.edu.co>
2026-08-24 21:42:10 -0500

    8

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -144,7 +144,7 @@ class Conversion:
             + unidades[numero % 10]
     )
     
-def romano_a_decimal(self, romano):
+    def romano_a_decimal(self, romano):
         """
         Convierte un número romano a decimal.
         
@@ -158,7 +158,30 @@ def romano_a_decimal(self, romano):
             romano_a_decimal("IX") -> 9
             romano_a_decimal("MCMXCIV") -> 1994
         """
-        pass
+        valores = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+        }
+
+        total = 0
+        anterior = 0
+
+        for letra in romano[::-1]:
+            valor = valores[letra]
+
+            if valor < anterior:
+                total -= valor
+            else:
+                total += valor
+
+            anterior = valor
+
+        return total
     
 def texto_a_morse(self, texto):
         """
