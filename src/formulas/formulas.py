0b4f23f557bd0b51a499d8b2bdd61045568a74b5
andres cabrera <afcabrera36@ucatolica.edu.co>
2026-08-24 23:24:35 -0500

    12

--- src/formulas/formulas.py
+++ src/formulas/formulas.py
@@ -227,7 +227,16 @@ class Formulas:
         Ejemplo:
             raices_cuadraticas(1, -3, 2) -> (2.0, 1.0)
         """
-        pass
+        discriminante = b ** 2 - 4 * a * c
+
+        if discriminante < 0:
+            raise ValueError("El discriminante es negativo")
+
+        raiz1 = (-b + discriminante ** 0.5) / (2 * a)
+        raiz2 = (-b - discriminante ** 0.5) / (2 * a)
+
+        return (raiz1, raiz2)
+        
 
     def imc(self, peso, altura):
         """
