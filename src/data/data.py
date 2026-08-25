9bf2d82409b83f0d9462439a8cb929ebd5a09dad
andres cabrera <afcabrera36@ucatolica.edu.co>
2026-08-24 21:58:48 -0500

    2

--- src/data/data.py
+++ src/data/data.py
@@ -33,7 +33,11 @@ class Data:
         Returns:
             int: Índice del elemento o -1 si no se encuentra
         """
-        pass
+        for i in range(len(lista)):
+            if lista[i] == elemento:
+                return i
+
+        return -1
     
     def eliminar_duplicados(self, lista):
         """
