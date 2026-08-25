b018adda24ef8d32d9db44ede6320c22088a8654
andres cabrera <afcabrera36@ucatolica.edu.co>
2026-08-24 21:57:39 -0500

    ejercicios data 1

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -244,7 +244,7 @@ class Conversion:
 
         return " ".join(resultado)
     
-def morse_a_texto(self, morse):
+    def morse_a_texto(self, morse):
         """
         Convierte código Morse a texto.
         
@@ -258,4 +258,49 @@ def morse_a_texto(self, morse):
             morse_a_texto("... --- ...") -> "SOS"
             morse_a_texto(".... . .-.. .-.. ---") -> "HELLO"
         """
-        pass
+        morse_dict = {
+        ".-": "A",
+        "-...": "B",
+        "-.-.": "C",
+        "-..": "D",
+        ".": "E",
+        "..-.": "F",
+        "--.": "G",
+        "....": "H",
+        "..": "I",
+        ".---": "J",
+        "-.-": "K",
+        ".-..": "L",
+        "--": "M",
+        "-.": "N",
+        "---": "O",
+        ".--.": "P",
+        "--.-": "Q",
+        ".-.": "R",
+        "...": "S",
+        "-": "T",
+        "..-": "U",
+        "...-": "V",
+        ".--": "W",
+        "-..-": "X",
+        "-.--": "Y",
+        "--..": "Z",
+        "-----": "0",
+        ".----": "1",
+        "..---": "2",
+        "...--": "3",
+        "....-": "4",
+        ".....": "5",
+        "-....": "6",
+        "--...": "7",
+        "---..": "8",
+        "----.": "9"
+        }
+
+        resultado = ""
+
+        for codigo in morse.split():
+            if codigo in morse_dict:
+                resultado += morse_dict[codigo]
+
+        return resultado
