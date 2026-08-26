2c394a23b42222290f4762ea50b62448d90d2e9f
andres cabrera <afcabrera36@ucatolica.edu.co>
2026-08-25 20:21:33 -0500

    9

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -156,7 +156,7 @@ class Geometria:
         Returns:
             float: Área del pentágono regular
         """
-        pass
+        return (5 * lado * apotema) / 2 
     
     def perimetro_pentagono_regular(self, lado):
         """
