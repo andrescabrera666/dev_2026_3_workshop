47dc29416470fc39a8a8e64dc793fc46da99bca7
andres cabrera <afcabrera36@ucatolica.edu.co>
2026-08-25 20:03:41 -0500

    4

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -75,7 +75,11 @@ class Geometria:
         Returns:
             float: Área del triángulo
         """
-        pass
+        if base < 0 or altura < 0:
+            return 0 
+
+        resultado = (base * altura) / 2
+        return resultado
     
     def perimetro_triangulo(self, lado1, lado2, lado3):
         """
