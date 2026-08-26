72b0be6f6266b2c1ae80d899475cacdfafac0748
andres cabrera <afcabrera36@ucatolica.edu.co>
2026-08-25 20:24:53 -0500

    12

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -193,8 +193,8 @@ class Geometria:
         Returns:
             float: Perímetro del hexágono regular
         """
-        pass
-    
+        return 6 * lado 
+        
     def volumen_cubo(self, lado):
         """
         Calcula el volumen de un cubo.
